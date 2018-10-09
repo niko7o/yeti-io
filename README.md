7cdf17f5251f78bfd6ef6923360ae1643d5bb6f0
Nikolaas <nikotomad@gmail.com>
2018-10-09 16:43:24 +0200

    build version 1 success

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yeti.io
+
+Yeti.io is a twitter-like social network built with React & Firebase including realtime feed/posts
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <nav>
+                <p>Navbar</p>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
