76438b83209ef4686920097823e33008a044b045
Nikolaas <nikotomad@gmail.com>
2018-10-09 16:37:17 +0200

    Updates

---
+++ precache-manifest.2fceba8cf8db3cfbe2234907e36ce14b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3021edbb8a1c8148f4b8",
+    "url": "/yeti-io/static/js/runtime~main.3021edbb.js"
+  },
+  {
+    "revision": "394e0331a2648104e498",
+    "url": "/yeti-io/static/js/main.394e0331.chunk.js"
+  },
+  {
+    "revision": "59f428ab3e64591f6a84",
+    "url": "/yeti-io/static/js/1.59f428ab.chunk.js"
+  },
+  {
+    "revision": "394e0331a2648104e498",
+    "url": "/yeti-io/static/css/main.2b8ecaaf.chunk.css"
+  },
+  {
+    "revision": "73842f5ecb5683fd0ef09fc3c029477d",
+    "url": "/yeti-io/index.html"
+  }
+];
